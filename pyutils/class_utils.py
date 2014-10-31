866720d67e7fb3cd2503289053d556d866fad62c
Veit Heller <veit.heller@student.htw-berlin.de>
2014-11-01 00:21:19 +0100

    Updated class utilities

---
+++ pyutils/class_utils.py
@@ -0,0 +1,15 @@
+class Closer:
+    '''A context manager to automatically close an object with a close method
+    in a with statement.'''
+
+    def __init__(self, obj):
+        self.obj = obj
+
+    def __enter__(self):
+        return self.obj
+
+    def __exit__(self, exception_type, exception_val, trace):
+        try:
+           self.obj.close()
+        except AttributeError:
+           pass
