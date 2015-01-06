128f4559406e5213bc9020cb0c7520e2c02c72f2
Veit Heller <veit.heller@student.htw-berlin.de>
2015-01-06 18:47:30 +0100

    Added optimizing metaclass

--- pyutils/class_utils.py
+++ pyutils/class_utils.py
@@ -1,3 +1,5 @@
+from decorators import _make_constants
+
 class Closer:
     '''A context manager to automatically close an object with a close method
     in a with statement.'''
@@ -13,3 +15,30 @@ class Closer:
            self.obj.close()
         except AttributeError:
            pass
+
+class LookupOptimizingMetaclass(type):
+    '''A metaclass that uses the _make_constants function to optimize
+       out repeated function lookups.'''
+    def __init__(cls, name, bases, dict):
+        super(OptimizingMetaclass, cls).__init__(name, bases, dict)
+
+        import types
+        for name, attribute in dict.items():
+            if type(attribute) is types.FunctionType:
+                dict[name] = _make_constants(attribute)
+
+def build_optimizing_metaclass(builtin_only=False, stoplist=[], verbose=False):
+    '''Factory function that builds a configurable optimizing metaclass.'''
+    from types import FunctionType
+    class _OptimizingMetaclass(type):
+        def __init__(cls, name, bases, dict):
+            super(_OptimizingMetaclass, cls).__init__(name, bases, dict)
+
+            for name, attribute in dict.items():
+                if type(attribute) is FunctionType:
+                    dict[name] = _make_constants(attribute, builtin_only, stoplist, verbose)
+
+    return _OptimizingMetaclass
+
+__metaclass__ = build_optimizing_metaclass(verbose=True)
+
