85c92933c02d2e8c2e02ddd98afa3d51ac77a1dd
Veit Heller <veit.heller@student.htw-berlin.de>
2015-01-07 14:08:37 +0100

    Added synchronized decorators

--- pyutils/decorators.py
+++ pyutils/decorators.py
@@ -1,4 +1,9 @@
-import tempfile, shutil
+import tempfile
+import shutil
+import types
+import _thread
+import threading
+
 from datetime import datetime
 from functools import wraps, partial
 from contextlib import contextmanager
@@ -148,6 +153,70 @@ def overrides(interface_class):
         assert(method.__name__ in dir(interface_class))
         return method
     return overrider
+
+def synchronized_with_attr(lock_name):
+    """Synchronized keyword decorator(see below)
+       that is using a premade lock that one can supply."""
+    def decorator(method):
+
+        def synced_method(self, *args, **kws):
+            lock = getattr(self, lock_name)
+            with lock:
+                return method(self, *args, **kws)
+
+        return synced_method
+
+    return decorator
+
+def syncronized_with(lock):
+    """Decorator factory for the synchronized
+       keyword(see below)."""
+
+    def synchronized_obj(obj):
+
+        if type(obj) is types.FunctionType:
+
+            obj.__lock__ = lock
+
+            def func(*args, **kws):
+                with lock:
+                    obj(*args, **kws)
+            return func
+
+        elif isinstance(obj, type):
+
+            orig_init = obj.__init__
+            def __init__(self, *args, **kws):
+                self.__lock__ = lock
+                orig_init(self, *args, **kws)
+            obj.__init__ = __init__
+
+            for key in obj.__dict__:
+                val = obj.__dict__[key]
+                if type(val) is types.FunctionType:
+                    decorator = syncronized_with(lock)
+                    setattr(obj, key, decorator(val))
+
+            return obj
+
+    return synchronized_obj
+
+def synchronized(item):
+    """Adds a synchronized primitive to something,
+       similar to the Java keyword."""
+
+    if type(item) is str:
+        decorator = synchronized_with_attr(item)
+        return decorator(item)
+
+    if type(item) is _thread.LockType:
+        decorator = syncronized_with(item)
+        return decorator(item)
+
+    else:
+        new_lock = threading.Lock()
+        decorator = syncronized_with(new_lock)
+        return decorator(item)
 
 
 from opcode import opmap, HAVE_ARGUMENT, EXTENDED_ARG
